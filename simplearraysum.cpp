88ffb624726cc854e69decfc17c034dc0eb32afe
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-05 23:23:48 +0530

    Create simplearraysum.cpp

---
+++ simplearraysum.cpp
@@ -0,0 +1,20 @@
+//https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+for(int i=0;i<n;i++)
+{
+   sum=sum+a[i]; 
+}
+cout<<sum;
+
+return 0;
+}
